c7d9ae5d30ebf217f9259d493f8b45a494e59f85
ivanvarghese <ivanvarghese@me.com>
2013-06-29 14:06:49 -0500

    Add routes and links for subpages for About page.
    God damn so frustrating. Code is ugly as hell and the CSS is buggy. Tears.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 TmcyfBackend::Application.routes.draw do
   devise_for :users
-  match '/events', to: 'static_pages#events',         via: 'get'
-  match '/about', to: 'static_pages#about',           via: 'get'
-  match '/biblestudy', to: 'static_pages#biblestudy', via: 'get'
+  match '/events', to: 'static_pages#events',                               via: 'get'
+  match '/about', to: 'static_pages#about',                                 via: 'get'
+  match '/about/biblestudy', to: 'static_pages#about_biblestudy',           via: 'get'
+  match '/about/tribes', to: 'static_pages#about_tribes',                   via: 'get'
+  match '/about/service', to: 'static_pages#about_service',                 via: 'get'
+  match '/about/socials', to: 'static_pages#about_socials',                 via: 'get'
+  match '/about/officers', to: 'static_pages#about_officers',               via: 'get'
+  match '/about/contact', to: 'static_pages#about_contact',                 via: 'get'
+  match '/biblestudy', to: 'static_pages#biblestudy',                       via: 'get'
 
   root :to => "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
