3532d2abe36ab71fc87192bb642270dd00a9afec
ivanvarghese <ivanvarghese@me.com>
2013-07-13 01:06:28 -0500

    Idk man

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,14 @@ Static::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-  :address   => "smtp.mandrillapp.com",
-  :port      => 587,
-  :user_name => ENV["admin@tmcyf.org"],
-  :password  => ENV["TpHCUC2nxNx45nMiJ-zNlg"]
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "tmcyf.org",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["admin@tmcyf.org"],
+  password: ENV["2corinth"]
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
