60680592c3d0d50c810d5a9f24ad236ba3783e81
ivanvarghese <ivanvarghese@me.com>
2013-07-13 00:53:51 -0500

    Add Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,21 @@ Static::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'thawing-bayou-8869.herokuapp.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :user_name => ENV["admin@tmcyf.org"],
+  :password  => ENV["TpHCUC2nxNx45nMiJ-zNlg"]
+}
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
