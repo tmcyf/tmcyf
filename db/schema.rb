1142e0e90b69e11921adb789349ecc41d3f81f13
ivanvarghese <ivanvarghese@me.com>
2013-07-14 02:47:00 -0500

    Added user profile with data persistence

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20130709182004) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130714072302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +33,20 @@ ActiveRecord::Schema.define(version: 20130709182004) do
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "fname"
+    t.string   "lname"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "gender"
+    t.date     "birthday"
+    t.decimal  "cumulative_amount_paid"
+    t.string   "line1"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
---
+++ lib/user_sanitizer.rb
@@ -0,0 +1,6 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+	private
+    def account_update
+        default_params.permit(:fname, :lname, :email, :password, :password_confirmation, :current_password, :line1, :city, :state, :zip, :phone, :gender, :birthday)
+    end
+end
