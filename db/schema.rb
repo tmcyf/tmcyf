2c483a3c3f7c478990657afeb4426d770de02d9e
ivanvarghese <ivanvarghese@me.com>
2013-07-17 23:19:29 -0500

    Add shirt size option to personal profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130714072302) do
+ActiveRecord::Schema.define(version: 20130718035011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20130714072302) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.string   "shirtsize"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
