5082ccefb4cbc7a1e32130f6eee57a485bd2bfb0
ivanvarghese <ivanvarghese@me.com>
2013-07-24 13:04:46 -0500

    Added barebones event page with create, edit, and delete functionality.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,42 @@ class EventsController < ApplicationController
 	def show
 		@event = Event.find(params[:id])
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		@event.save
+
+		redirect_to events_path
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		@event.update(event_params)
+
+		flash.notice = "Event '#{@event.title}' Updated!"
+
+		redirect_to events_path
+	end
+
+
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+
+		redirect_to events_path
+	end
+
+
+
+	def event_params
+		params.require(:event).permit(:title, :body, :date, :time, :location)
+	end	
 end
