eb40ba371d5cb600cb25ab451d939bbda9bbe8fa
ivanvarghese <ivanvarghese@me.com>
2013-07-27 12:51:44 -0500

    Removed and consolidated migrations. Fixed gibbon error handling.

--- db/migrate/20130723203055_add_contact_preferences_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddContactPreferencesToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :email_contact, :boolean
-    add_column :users, :facebook_contact, :boolean
-    add_column :users, :sms_contact, :boolean
-  end
-end
