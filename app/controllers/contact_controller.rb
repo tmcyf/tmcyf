3bdf503e5689dd73e3ba9e47caae454ffa85bc59
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-07-27 13:35:43 -0500

    Bugfixes regarding contact preferences.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,15 +8,15 @@ class ContactController < ApplicationController
   def send_all_message
     message = params[:message]
     numbers_to_sms = User.where(sms_contact: true).pluck(:phone)
-    numbers_to_sms.each { |number| self.text_contact(message, number) }
+    numbers_to_sms.each { |number| text_contact(message, number) }
     redirect_to contact_all_path
   end
 
   private
   def text_contact(message, contact_number)
-    @twilio_client = Twilio::REST::Client.new ENV[TWILIO_SID], ENV[TWILIO_TOKEN]
+    @twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
     @twilio_client.account.sms.messages.create(
-      :from => "+1#{ENV[TWILIO_NUMBER]}",
+      :from => "+1#{ENV['TWILIO_NUMBER']}",
       :to => contact_number,
       :body => message
     )
