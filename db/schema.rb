f316e7f7be3c2e8ad1b181cb4b2e3727ea40daa2
ivanvarghese <ivanvarghese@me.com>
2013-07-27 15:01:20 -0500

    Added flash messages and redirect for after confirmation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727002757) do
+ActiveRecord::Schema.define(version: 20130724175751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,24 +20,15 @@ ActiveRecord::Schema.define(version: 20130727002757) do
     t.string   "title"
     t.date     "date"
     t.time     "time"
-    t.string   "location"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text     "location"
     t.string   "image"
     t.text     "body"
     t.string   "slug"
-  end
-
-  add_index "events", ["slug"], name: "index_events_on_slug", using: :btree
-
-  create_table "payments", force: true do |t|
-    t.string   "label"
-    t.decimal  "amount"
-    t.integer  "last_four"
-    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "events", ["slug"], name: "index_events_on_slug", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -61,7 +52,6 @@ ActiveRecord::Schema.define(version: 20130727002757) do
     t.string   "phone"
     t.string   "gender"
     t.date     "birthday"
-    t.decimal  "cumulative_amount_paid"
     t.string   "line1"
     t.string   "city"
     t.string   "state"
