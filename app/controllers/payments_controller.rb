54304e46348313d43ce740a2d04d1bc17db3a091
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-07-28 17:02:52 -0500

    Add cost value to events, parameterize payments index
    - also refactored array#inject call in credit_card controller to array#collect

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,10 @@
 class PaymentsController < ApplicationController
+  def index
+    events_requiring_payment = Event.where.not(cost: nil)
+    events_paid_for = current_user.payments.collect { |p| p.event }
+    # the set difference of two arrays a & b in ruby is a - b | b - a
+    @unpaid_events = events_requiring_payment - events_paid_for | events_paid_for - events_requiring_payment
+  end
   def new
     # in this page, you should only be able to pick credit cards that are valid
     # for current_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728205225) do
+ActiveRecord::Schema.define(version: 20130728220126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20130728205225) do
     t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "cost"
   end
 
   add_index "events", ["slug"], name: "index_events_on_slug", using: :btree
