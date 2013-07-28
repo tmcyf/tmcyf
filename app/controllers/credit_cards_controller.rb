4c981808c9ae0b406e9a682b62fa261845ca7c18
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-07-28 16:14:13 -0500

    Stubbed out credit card and payment controller methods

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,2 +1,18 @@
 class CreditCardsController < ApplicationController
+  def create
+    # set credit card user to current_user
+    # create a stripe::customer with the card
+    # save stripe token 
+  end
+
+  def index
+    @card_tokens = current_user.credit_cards
+    @card_tokens.inject([]) do |token, array_of_digits|
+      # grab the last four digits of the card token via the Stripe API 
+      # and add it to the digits array
+    end
+  end
+
+  def destroy
+  end
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,13 @@
 class PaymentsController < ApplicationController
+  def new
+    # in this page, you should only be able to pick credit cards that are valid
+    # for current_user
+    @payment = Payment.new
+  end
+  def create
+    # does this method know that we're paying for the user currently signed in?
+    # :stripe_token is set by Stripe.js, which we handle in credit_card.js.coffee
+    @payment.credit_card = CreditCard.find_by(stripe_token: params[:stripe_token])
+    @payment.event = params[:event_id]
+  end
 end
