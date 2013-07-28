4e0164e05405c1baedf1cfc4203c21e74d5dcadf
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-07-28 16:31:48 -0500

    Confirmed that credit_cards.js.coffee code was available on payments page

--- app/assets/javascripts/credit_cards.js.coffee
+++ app/assets/javascripts/credit_cards.js.coffee
@@ -1,3 +1,5 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery ->
+    alert "YO! DIS JS COME FROM credit_cards.js.coffee! DO WORK"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728202302) do
+ActiveRecord::Schema.define(version: 20130728205225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130728202302) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_token"
   end
 
   create_table "events", force: true do |t|
