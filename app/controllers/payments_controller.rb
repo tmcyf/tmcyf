34b80ada6ae4a96856a6e8795a0548cef97a52a4
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-07-28 16:31:48 -0500

    Confirmed that credit_cards.js.coffee code was available on payments page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,12 +2,15 @@ class PaymentsController < ApplicationController
   def new
     # in this page, you should only be able to pick credit cards that are valid
     # for current_user
+    @usable_cards = current_user.credit_cards
     @payment = Payment.new
+    # the user is directed to this page by clicking "Pay Now" on a payment on
+    # the payments/index page (at the route /account/payments)
+    @payment.event = params[:event_id]
   end
   def create
     # does this method know that we're paying for the user currently signed in?
     # :stripe_token is set by Stripe.js, which we handle in credit_card.js.coffee
     @payment.credit_card = CreditCard.find_by(stripe_token: params[:stripe_token])
-    @payment.event = params[:event_id]
   end
 end
