3fdc97455f8fd9e4b0570e49dd7d547678043d61
ivanvarghese <ivanvarghese@me.com>
2013-08-17 23:54:17 -0500

    Added names of facebook users in admin page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
   
   def fullname
-  	self.fname + self.lname
+  	self.fname + " " + self.lname
   end
 
   def address
