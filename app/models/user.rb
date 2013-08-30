3dfdf8038f1a9c7be3059b03cb788ec8ecb6ed61
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-08-30 14:23:52 -0500

    Fixed new_card failing to update the Stripe customer's default card.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ActiveRecord::Base
       raise "No stripe token submitted" unless stripe_token
       if self.stripe_id
         customer = Stripe::Customer.retrieve(self.stripe_id)
-        customer.cards.create(card: stripe_token)
+        customer.default_card = customer.cards.create(card: stripe_token)
+        customer.save
       else
         customer = Stripe::Customer.create(
           description: self.fullname,
