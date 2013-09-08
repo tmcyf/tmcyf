139e2a3f4a611bbb97a97ca275cc294e11ac79a2
ivanvarghese <ivanvarghese@me.com>
2013-09-08 10:33:00 -0500

    Fixed merge errors. Develop and master should match exactly.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:home, :about, :about_biblestudies, :about_tribes, :about_service, :about_socials, :about_officers, :about_contact, :events, :biblestudy, :account]
+  before_filter :authenticate_user!, :except => [:home, :about, :about_biblestudies, :about_tribes, :about_service, :about_socials, :about_officers, :about_contact, :events, :biblestudy, :privacy_policy]
 
   def home
     url = 'http://www.heartlight.org/cgi-shl/todaysverse.cgi'
@@ -15,25 +15,36 @@ class PagesController < ApplicationController
   end
 
   def account
+    @completion_percentage = current_user.profile_completion_percentage
+    @paid_events = current_user.paid_events
+    @dues_paid = current_user.dues_paid?
+    @has_selected_contact_preference = current_user.has_selected_contact_preference?
   end
 
   def admin
+    redirect_to :root unless current_user && current_user.admin?
+    @fbname = User.where(facebook_contact: true).collect { |user| user.fullname }
   end
 
   def preferences
     @user = current_user
   end
+
+  def privacy_policy
+  end
+
   def update_preferences
     @user = current_user
     params['Facebook'] ? @user.facebook_contact=true : @user.facebook_contact=false
-    params["email"] ? @user.email_subscribe : @user.email_unsubscribe 
+    params["email"] ? @user.email_subscribe : @user.email_unsubscribe
     if params["SMS"]
-      error_msg = "There was a problem subscribing you to our text alerts. Is there a valid phone number in your Profile?" 
-      flash[:error] = error_msg unless @user.sms_subscribe 
+      error_msg = "There was a problem subscribing you to our text alerts. Is there a valid phone number in your Profile?"
+      flash[:error] = error_msg unless @user.sms_subscribe
     else
       @user.sms_unsubscribe
     end
     @user.save!
+    flash[:notice] = "Preferences saved!"
     redirect_to account_preferences_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ActiveRecord::Base
   has_many :payments
 
   def fullname
-  	self.fname ? self.fname + " " + self.lname : nil
+    self.fname ? self.fname + " " + self.lname : nil
   end
 
   def address
-  	self.line1 + self.city + self.state + self.zip
+    self.line1 + self.city + self.state + self.zip
   end
 
   def new_card(stripe_token)
@@ -154,28 +154,5 @@ class User < ActiveRecord::Base
     end
     # calculate the average, round the result
     ((completed / total)* 100).round 
-  end
-
-  def email_subscribe
-    gibbon = Gibbon::API.new
-    Gibbon::API.throws_exceptions = false
-    self.email_contact = true 
-    if !gibbon.lists.members(id: "4c04c52ede", email: self.email)
-      gibbon.lists.subscribe({:id => "4c04c52ede", :email => {:email => self.email}, :merge_vars => {:FNAME => self.fname, :LNAME => self.lname}, :double_optin => false})
-    end
-  end
-  def email_unsubscribe
-    gibbon = Gibbon::API.new
-    Gibbon::API.throws_exceptions = false
-    self.email_contact = false 
-    if gibbon.lists.members(id: "4c04c52ede", email: self.email)
-      gibbon.lists.unsubscribe({:id => "4c04c52ede", :email => {:email => self.email}, :merge_vars => {:FNAME => self.fname, :LNAME => self.lname}, :double_optin => false})
-    end
-  end
-  def sms_subscribe
-    self.phone? ? self.sms_contact = true : false
-  end
-  def sms_unsubscribe
-    self.sms_contact=false
   end
 end
