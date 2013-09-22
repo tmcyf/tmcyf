b7429478b090d45c56baf1e6a0d2e5f61e73592c
ivanvarghese <ivanvarghese@me.com>
2013-09-22 01:39:50 -0500

    Added donation page.

--- app/controllers/give_controller.rb
+++ app/controllers/give_controller.rb
@@ -1,6 +1,33 @@
 class GiveController < ApplicationController
-	before_filter :authenticate_user!, :except => [:index]
+	before_filter :authenticate_user!, :except => [:index, :new, :create]
 
-	def index		
+	def index
+	end
+
+	def new		
+	end
+
+	def create
+  @amount = params['amount'].to_i * 100
+
+
+  customer = Stripe::Customer.create(
+    :email => 'donation@tmcyf.org',
+    :card =>  params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+  )
+  flash[:success] = "Swaggg!"
+  redirect_to give_index_path
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to give_index_path		
+		
 	end
 end
