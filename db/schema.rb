6aba6f77afc05e1821adf77e96164f7c7181e01d
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-10-17 23:04:32 -0500

    Fixed Retreat model being mixed with Event model, stubbed out methods for current retreat and checks for user retreat registration and retreat payment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131018031858) do
+ActiveRecord::Schema.define(version: 20131018040023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20131018031858) do
     t.datetime "startdt"
     t.datetime "enddt"
     t.boolean  "dues"
+    t.string   "type"
   end
 
   add_index "events", ["slug"], name: "index_events_on_slug", using: :btree
