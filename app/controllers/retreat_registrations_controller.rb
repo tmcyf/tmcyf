e8ce72a5ed7dcf22dfeb5000c58fede83462ec5a
ivanvarghese <ivanvarghese@me.com>
2013-10-19 23:10:47 -0500

    Axed trailing whitespaces.

--- app/controllers/retreat_registrations_controller.rb
+++ app/controllers/retreat_registrations_controller.rb
@@ -12,15 +12,15 @@ class RetreatRegistrationsController < ApplicationController
   end
 
   private
-  def registration_params 
-    params.require(:retreat_registration).permit(:fname, :lname, :line1, :city, 
+  def registration_params
+    params.require(:retreat_registration).permit(:fname, :lname, :line1, :city,
                                                  :state, :zip, :phone, :gender,
-                                                 :birthday, :shirtsize, 
-                                                 :emergency_contact, 
-                                                 :emergency_phone, 
-                                                 :insurance_provider, 
-                                                 :emergency_relation, 
-                                                 :insurance_policy_number, 
+                                                 :birthday, :shirtsize,
+                                                 :emergency_contact,
+                                                 :emergency_phone,
+                                                 :insurance_provider,
+                                                 :emergency_relation,
+                                                 :insurance_policy_number,
                                                  :days_attending, :retreat_id)
   end
 end
--- spec/requests/retreats_spec.rb
+++ spec/requests/retreats_spec.rb
@@ -4,7 +4,7 @@ describe "Retreats" do
   describe "GET /retreat" do
     it "displays the latest retreat if a retreat has been created" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      
+
       FactoryGirl.create :retreat
       get retreat_path
       response.status.should be(200)
