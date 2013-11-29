4776106b647c6eba5b7293675112fd6741166e74
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-11-29 16:36:37 -0600

    Finalized cherry-picking from refunds branch.
    Decided to cherry-pick commits from the csv and refunds branch instead of a full blown merge to avoid the strange develop -> testing, develop -> refactor-css, and develop -> develop merges. Good idea, Bert.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,27 @@
 FactoryGirl.define do
-  factory :user do |f|
-    f.sequence(:email) { |n| "foo#{n}@example.com" }
-    f.password("secret12")
+  factory :user do
+    sequence(:email) { |n| "foo#{n}@example.com" }
+    password("secret12")
     # might want to consider making this optional, having "should confirm" as
     # an argument to the factory call
-    f.confirmed_at(DateTime.now)
+    confirmed_at(DateTime.now)
   end
-  
-  factory :retreat do |f|
-    f.sequence(:title, 2013) {|n| "Retreat #{n}" }
-    f.location("Houston")
-    f.body("A retreat.")
-    f.created_at(DateTime.now)
-    f.startdt(DateTime.now)
-    f.enddt(DateTime.now + 3.days)
+
+  factory :retreat do
+    sequence(:title, 2013) {|n| "Retreat #{n}" }
+    location "Houston"
+    body "A retreat."
+    created_at DateTime.now
+    startdt DateTime.now
+    enddt DateTime.now + 3.days
+  end
+  factory :retreat_registration do
+    user
+    retreat
+    emergency_contact "prolly ivan"
+    emergency_phone "7139683673"
+    emergency_relation "prolly ur mom"
+    insurance_policy_number "8675309"
+    days_attending 2
   end
 end
