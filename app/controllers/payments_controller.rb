a02308d5c53c741908dfd2ee3924fb2047b3b111
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-01-01 19:00:38 -0600

    Fix nil credit card crashing app (payments#create)

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,22 +8,27 @@ class PaymentsController < ApplicationController
     @payment = Payment.new
   end
   def create
-    @payment = Payment.create # should include the event id & current user
-    @payment.event = Event.friendly.find(params[:event_id])
-    @payment.last4 = current_user.credit_card.last4 # TODO: cache this value
-    # TODO: I think this is an anti-pattern? Try current_user.payment.build or
-    # something to that effect
-    @payment.user = current_user
-    begin
-      @payment.stripe_id = current_user.charge(amount: (@payment.event.cost * 100).to_i,
-                          description: "Payment for TMCYF #{@payment.event.title}").id
-      flash[:success] = "Payment has been made for #{@payment.event.title}" if @payment.save!
+    unless current_user.credit_card
+      flash[:error] = "Please provide a valid credit card"
       redirect_to account_payments_path
-    rescue Stripe::CardError => e
-      # TODO: More informative error messages
-      logger.info(e)
-      flash[:error] = e.json_body[:message]
-      redirect_to account_payments_path
+    else
+      @payment = Payment.create # should include the event id & current user
+      @payment.event = Event.friendly.find(params[:event_id])
+      @payment.last4 = current_user.credit_card.last4 # TODO: cache this value
+      # TODO: I think this is an anti-pattern? Try current_user.payment.build or
+      # something to that effect
+      @payment.user = current_user
+      begin
+        @payment.stripe_id = current_user.charge(amount: (@payment.event.cost * 100).to_i,
+                            description: "Payment for TMCYF #{@payment.event.title}").id
+        flash[:success] = "Payment has been made for #{@payment.event.title}" if @payment.save!
+        redirect_to account_payments_path
+      rescue Stripe::CardError => e
+        # TODO: More informative error messages
+        logger.info(e)
+        flash[:error] = e.json_body[:message]
+        redirect_to account_payments_path
+      end
     end
   end
 
