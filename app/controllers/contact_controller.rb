d979f1e108458a4349625e07dcedd994dc74ce66
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-01-08 00:12:35 -0600

    Not working still

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -22,19 +22,28 @@ class ContactController < ApplicationController
   end
 
   def send_all_message
-    message_chunks = params[:message].scan(/.{155}/)
-    numbers_to_sms = User.where(sms_contact: true).pluck(:phone)
     # if any of these numbers are nil, the text_contact call will fail and
     # return false
-    numbers_to_sms.each do |number| 
-      message_chunks.each_with_index do |message, index|
-        text_contact(message + "#{index}/#{message_chunks.length}", number)
+    numbers_to_sms = User.where(sms_contact: true).pluck(:phone)
+    if params[:message].length > 160
+      numbers_to_sms.each do |number| 
+        message_chunks.each_with_index do |message, index|
+          text_contact(message + "#{index+1}/#{message_chunks.length}", number)
+        end
+      end
+    else
+      numbers_to_sms.each do |number| 
+        text_contact(message, number)
       end
     end
     redirect_to send_sms_path
   end
 
   private
+  def message_chunks
+    # need to leave 5 chars for the msg index (e.g. "5/6")
+    params[:message].scan(/.{155}/)
+  end
   def text_contact(message, contact_number)
     if contact_number
       @twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
