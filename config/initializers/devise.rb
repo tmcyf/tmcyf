71129324e36f2cdb52743e9c5b677e59c5397f37
Ivan Varghese <ivanvarghese@me.com>
2014-01-22 01:36:07 -0600

    updated devise.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "no-reply@tmcyf.org"
 
+  config.secret_key = "#{ENV['SECRET_KEY_BASE}']}"
+
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
 
