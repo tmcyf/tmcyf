0f4438421e833e9cb5a71d4a2594e7866d132171
Ivan Varghese <ivanvarghese@me.com>
2014-01-28 17:50:35 -0600

    Added archive page with last years committee bio.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:home, :about, :about_biblestudies, :about_tribes, :about_service, :about_socials, :about_officers, :about_contact, :events, :biblestudy, :privacy_policy]
+  before_filter :authenticate_user!, :except => [:home, :about, :about_biblestudies, :about_tribes, :about_service, :about_socials, :about_officers, :officers_archive, :about_contact, :events, :biblestudy, :privacy_policy]
 
   def home
     url = 'http://www.heartlight.org/cgi-shl/todaysverse.cgi'
