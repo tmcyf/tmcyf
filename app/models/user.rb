990bd3d37d5e40c6ff376e9bffa96f8267e47c5b
Ivan Varghese <ivanvarghese@me.com>
2014-01-30 13:53:39 -0600

    Damn you to hell hashrocket.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     self.phone = phone.gsub(/[^0-9]/, "") if attribute_present?("phone")
   end
 
-  validates_format_of :phone, :with => /\d*[1-9]\d*/i, :on => :update, message: "This isn't a valid number!", :allow_blank => true, :allow_nil = true
+  validates_format_of :phone, :with => /\d*[1-9]\d*/i, :on => :update, message: "This isn't a valid number!", :allow_blank => true, :allow_nil => true
   validates :phone, length: { is: 10, wrong_length: "Your number doesn't look valid. Check again!" }
 
   def fullname
