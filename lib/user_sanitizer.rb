421f10b31642e2890f4ab7963b5965198b2cf8d8
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-01-31 17:20:46 -0600

    Fix persistence failures for fname/lname on signup
    Also includes a bugfix for [#76](https://github.com/tmcyf/tmcyf/issues/76).

--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,9 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
 	private
     def account_update
-        default_params.permit(:fname, :lname, :email, :password, :password_confirmation, :current_password, :line1, :city, :state, :zip, :phone, :gender, :birthday, :shirtsize)
+      default_params.permit(:fname, :lname, :email, :password, :password_confirmation, :current_password, :line1, :city, :state, :zip, :phone, :gender, :birthday, :shirtsize)
+    end
+    def sign_up
+      default_params.permit(:fname, :lname, :email, :password, :password_confirmation)
     end
 end
