be0498381ef460cc9ac9e5e2e8be8f1137f96093
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-02-03 19:22:26 -0600

    Switch to Twilio messages API. Fix #81.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -25,30 +25,17 @@ class ContactController < ApplicationController
     # if any of these numbers are nil, the text_contact call will fail and
     # return false
     numbers_to_sms = User.where(sms_contact: true).pluck(:phone)
-    if params[:message].length > 160
-      numbers_to_sms.each do |number| 
-        message_chunks.each_with_index do |message, index|
-          text_contact(message + "(#{index+1}/#{message_chunks.length})", number)
-        end
-      end
-    else
-      numbers_to_sms.each do |number| 
-        text_contact(params[:message], number)
-      end
+    numbers_to_sms.each do |number| 
+      text_contact(params[:message], number)
     end
     redirect_to send_sms_path
   end
 
   private
-  def message_chunks
-    # message limit is 160 chars
-    # need to leave 6 chars for the msg index (e.g. "(5/6)" or "(5/10)")
-    params[:message].scan(/.{1,154}/)
-  end
   def text_contact(message, contact_number)
     if contact_number
       @twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-      @twilio_client.account.sms.messages.create(
+      @twilio_client.account.messages.create(
         :from => "+1#{ENV['TWILIO_NUMBER']}",
         :to => contact_number,
         :body => message
