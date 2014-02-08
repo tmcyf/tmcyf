2f2cb549341f6ec6e5fa0cd9bca76eb7342cd764
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-02-08 05:49:41 -0600

    Start work on xls export
    - set up to_xls method on User class
    - set up route and admin controller method
    - register mime type with the application
    - require csv in application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Static::Application.routes.draw do
                  sign_out: 'logout',
                  sign_up: 'register',
                  password: 'reset'}
+  get '/users/database.xls' => 'admin#xls'
 
   devise_scope :user do
     get '/login'                    => 'devise/sessions#new'
