59ca00b5ad92cbbd9ef4ca304ef2054b52548457
Ivan Varghese <ivanvarghese@me.com>
2014-02-26 19:18:31 -0600

    Strong params for devise
    Removed old way of sanitizing non-default user parameters which caused User::ParameterSanitizer errors. Took out unused confirmations controller.

--- lib/user_sanitizer.rb
+++
@@ -1,9 +0,0 @@
-class User::ParameterSanitizer < Devise::ParameterSanitizer
-	private
-    def account_update
-      default_params.permit(:fname, :lname, :email, :password, :password_confirmation, :current_password, :line1, :city, :state, :zip, :phone, :gender, :birthday, :shirtsize)
-    end
-    def sign_up
-      default_params.permit(:fname, :lname, :email, :password, :password_confirmation)
-    end
-end
