046a6abb9351cb98d311cba3795e02a1229281c3
Ivan Varghese <ivanvarghese@me.com>
2014-02-26 19:50:47 -0600

    Removed payments code.
    Removed payments and credit card code. All references to costs, dues, payments, credit cards have been taken out from controllers, models, and views. Removed costs and dues from events database and removed payments table. Deleted left over retreat asset files.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
-	has_many :payments
 	mount_uploader :image, ImageUploader
 	extend FriendlyId
 	# without :finders, we need to use Event.friendly.find in all cases
