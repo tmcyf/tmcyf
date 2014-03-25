bc9e43d395b8e6d70c9f60b3adb1a79bbfba282d
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-03-24 22:16:47 -0500

    Fix syntax bug from misapplied regex

--- app/controllers/give_controller.rb
+++ app/controllers/give_controller.rb
@@ -1,33 +1,33 @@
 class GiveController < ApplicationController
-	before_filter :authenticate_user!, except: [:index, :new, :create]
+  before_filter :authenticate_user!, except: [:index, :new, :create]
 
-	def index
-	end
+  def index
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-  @amount = params['amount'].to_i * 100
+  def create
+    @amount = params['amount'].to_i * 100
 
 
-  customer = Stripe::Customer.create(
-    email: 'donation@tmcyf.org',
-    card:  params[:stripeToken]
-  )
+    customer = Stripe::Customer.create(
+      email: 'donation@tmcyf.org',
+      card:  params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    customer: customer.id,
-    amount: @amount,
-    description: 'Rails Stripe customer',
-    currency: 'usd'
-  )
-  flash[:success] = "Thanks for your donation!"
-  redirect_to give_index_path
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+    flash[:success] = "Thanks for your donation!"
+    redirect_to give_index_path
 
-rescue Stripe:CardError: e
-  flash[:error] = e.message
-  redirect_to give_index_path
+  rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to give_index_path
 
-	end
+  end
 end
