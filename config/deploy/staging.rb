9b29162dcd4a28f20ae7fda8ef01ebc8290733ae
Ivan Varghese <ivanvarghese@me.com>
2014-04-12 11:56:05 -0500

    Added staging environment.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
 set :stage, :staging
-set :branch, 'master'
+set :branch, 'rubber-less-deploy'
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 set :site, "tmcyf.org"
-server '192.241.162.86', user: 'deploy', roles: %w{web app}, primary: true
+server '192.241.162.86', user: 'deploy', roles: %w{web app db}, primary: true
 
 # NGINX
 
