635dbd0373264acec5e068a24695a9dc37d976df
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-04-20 15:07:59 -0500

    Refactoring and some tests

--- spec/lib/sms_client_spec.rb
+++
@@ -1,9 +0,0 @@
-describe BatchSMS do
-  context 'with valid messages' do
-    it 'successfully sends twilio requests' do
-      let(:sms) { stub(message: "yo"
-                       number: "8675309") }
-      # TODO: everything lol
-    end
-  end
-end
