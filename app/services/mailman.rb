8ee8870b2f13b85d3aba49bf95ca79a1f7c9aead
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-04-25 09:04:17 -0500

    tabs -> spaces

--- app/services/mailman.rb
+++ app/services/mailman.rb
@@ -6,18 +6,17 @@ class Mailman
   end
 
   def subscribe(user)
-		if !mailchimp_member?(user)
-			@gibbon.lists.subscribe(id: @campaign_id,
+    if !mailchimp_member?(user)
+      @gibbon.lists.subscribe(id: @campaign_id,
           email: {email: user.email},
           merge_vars: {FNAME: user.fname, LNAME: user.lname},
           double_optin: false)
-		end
+    end
   end
 
   def unsubscribe
     if mailchimp_member?(user)
-			@gibbon.lists.unsubscribe(id: @campaign_id,
-					email: {email: user.email})
+      @gibbon.lists.unsubscribe(id: @campaign_id, email: {email: user.email})
     end
   end
 
