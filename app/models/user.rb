1e2b77a61c3049e0d9baea5555e315e4159f0f57
Ivan Varghese <ivanvarghese@me.com>
2014-04-24 21:33:58 -0500

    Removed auto opt-in feature.
    Automatically opting users into the mailing list isn't the right way to build a healthy mailing list. Unsolicited emails cause people to unsubscribe and drive down email opens. Even if we had to auto opt-in users, this current implementation is tightly coupled with Mailchimp. If, for example, Mailchimp's API isn't working or they are experiencing downtime, members cannot make a new account because of the after_create callback. The better way would be to do it in the background. Need to discuss user onboarding + initial user experience.

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,6 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
-  after_create :auto_optin
 
   before_validation(on: :create, on: :update) do
     self.phone = phone.gsub(/[^0-9]/, "") if attribute_present?("phone")
@@ -56,12 +55,6 @@ class User < ActiveRecord::Base
   scope :prefers_emails, -> { where(email_contact: true) }
   scope :prefers_sms, -> { where(sms_contact: true) }
   scope :prefers_fb, -> { where(facebook_contact: true) }
-
-  def auto_optin
-    self.email_contact = true
-    self.email_subscribe
-    self.save!
-  end
 
   def fullname
     "#{self.fname} #{self.lname}"
