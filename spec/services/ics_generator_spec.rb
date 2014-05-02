577b7678604377dcd0cd4ae68b88fbe2b73a5779
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-05-02 13:58:23 -0500

    Modify ics generator spec

--- spec/services/ics_generator_spec.rb
+++ spec/services/ics_generator_spec.rb
@@ -5,7 +5,7 @@ describe IcsGenerator do
     let(:event) { build(:event, title: 'Test Event') }
 
     it 'delegates to Icalendar' do
-      IcsGenerator::Calendar.should_receive(:new).and_call_original
+      IcsGenerator::Calendar.should_receive(:ics_for).and_call_original
       IcsGenerator.ics_for(event)
     end
   end
