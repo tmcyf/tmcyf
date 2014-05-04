8a18030e49aadd304c9c01a5df422a320240607e
Ivan Varghese <ivanvarghese@me.com>
2014-05-04 17:47:42 -0500

    Updated compenstation formula.

--- app/services/stripe_compensator.rb
+++ app/services/stripe_compensator.rb
@@ -1,7 +1,7 @@
 class StripeCompensator
   # Add domain logic to this file
   def self.compensate(amount)
-    amount = amount * 1.03 + 30
+    amount = (amount + 30)/0.971
     amount.to_i
   end
 
--- spec/services/stripe_compensator_spec.rb
+++ spec/services/stripe_compensator_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../app/services/stripe_compensator.rb'
 describe StripeCompensator do
   let(:original) { 500 }
   it 'adds 30c plus 3% to the given amount in cents' do
-    StripeCompensator.compensate(original).should == (500 * 1.03 + 30)
+    StripeCompensator.compensate(original).should == ((500 + 30)/ 0.971).to_i
   end
 
 end
