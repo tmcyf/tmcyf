d45c8d04e9a0cd70bf3ba971559672b9a3932dd2
Ivan Varghese <ivanvarghese@me.com>
2014-05-04 16:06:07 -0500

    Add validation for amount.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ActiveRecord::Base
   belongs_to :payable, polymorphic: true
   has_many :charges
+
+  validates :amount, :presence => true, :numericality => { :greater_than => 0 }
 end
