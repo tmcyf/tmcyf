762267ece81de9c713468f8c476db5c383854a1c
Ivan Varghese <ivanvarghese@me.com>
2016-04-30 01:18:50 -0500

    Making changes to codebase to comply with Ruby update.

--- app/models/mailman.rb
+++ app/models/mailman.rb
@@ -1,32 +1,58 @@
 class Mailman
   def initialize
-    @gibbon = Gibbon::API.new
+    @gibbon = Gibbon::Request.new
     @campaign_id = ENV['MAILCHIMP_CAMPAIGN_ID']
-    Gibbon::API.throws_exceptions = false
   end
 
   def subscribe(user)
+    @hashed_email = lower_case_and_md5_hash_email(user)
     if !mailchimp_member?(user)
-      @gibbon.lists.subscribe(id: @campaign_id,
-          email: {email: user.email},
-          merge_vars: {FNAME: user.fname, LNAME: user.lname},
-          double_optin: false)
+      @gibbon.lists(@campaign_id).members.create(
+        body: {
+          email_address: user.email,
+          status: "subscribed",
+          merge_fields:
+            { FNAME: user.fname, LNAME: user.lname }
+        }
+      )
+    else
+      @gibbon.lists(@campaign_id).members(@hashed_email).update(
+        body: {
+          status: "subscribed"
+        }
+      )
     end
     user.email_contact = true
   end
 
   def unsubscribe(user)
+    @hashed_email = lower_case_and_md5_hash_email(user)
     if mailchimp_member?(user)
-      @gibbon.lists.unsubscribe(id: @campaign_id,
-          email: {email: user.email})
+      @gibbon.lists(@campaign_id).members(@hashed_email).update(
+        body: {
+          status: "unsubscribed"
+        }
+      )
     end
     user.email_contact = false
   end
 
   def mailchimp_member?(user)
-    @gibbon.lists.members(id: @campaign_id)['data'].detect do |u|
-      u['email'] == user.email
+    @hashed_email = lower_case_and_md5_hash_email(user)
+    @gibbon.lists(@campaign_id).members.retrieve['members'].detect do |u|
+      u['id'] == @hashed_email
     end
   end
 
+  def get_list
+    @gibbon.lists(@campaign_id).members.retrieve
+  end
+
+  protected
+
+  def lower_case_and_md5_hash_email(user)
+    downcased_email = user.email.downcase
+    hashed_email = Digest::MD5.hexdigest(downcased_email)
+    hashed_email
+  end
 end
