5eb7f83ba7c1262957852e4c72f2bb8bb3c92c38
Ivan Varghese <ivanvarghese@me.com>
2017-05-30 08:08:33 -0500

    Removed VOTD temporarily per #205.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
 
   def home
-    url = 'http://www.heartlight.org/cgi-shl/todaysverse.cgi'
-    data = Nokogiri::HTML(open(url))
-    @votd = data.css(".well").children.first.text
-    @votdref = data.css(".well").children.last.text
+    # url = 'http://www.heartlight.org/cgi-shl/todaysverse.cgi'
+    # data = Nokogiri::HTML(open(url))
+    # @votd = data.css(".well").children.first.text
+    # @votdref = data.css(".well").children.last.text
 
     @featured_event = FeaturedEvent.last
   end
