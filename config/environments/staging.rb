a861cbf5bc711a34ccadf40c69e1553d39850225
Ivan Varghese <ivanvarghese@me.com>
2019-03-14 14:08:38 -0500

    Replaced SMTP sending for transactional emails.
    Basic authentication through the API or SMTP for users who have 2FA enabled will no longer be accepted per SendGrid. Added an ActionMailer adapter allowing emails to be sent via web api instead of SMTP.`

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,15 +75,21 @@ TMCYF::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.sendgrid.net",
+  #   port: 587,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["SENDGRID_USERNAME"],
+  #   password: ENV["SENDGRID_PASSWORD"]
+  # }
 
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY'],
+    raise_delivery_errors: true
+  }
+  
   config.assets.cache_store = :memory_store
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
