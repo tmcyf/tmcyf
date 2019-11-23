866d5d6cb636d8aa24c5c80612ad20868044f05f
Ivan Varghese <ivanvarghese@me.com>
2019-11-23 16:05:25 -0600

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 20161107022026) do
   enable_extension "plpgsql"
 
   create_table "charges", force: :cascade do |t|
-    t.string  "stripe_id",  limit: 255
-    t.string  "last4",      limit: 255
+    t.string  "stripe_id"
+    t.string  "last4"
     t.decimal "amount"
     t.integer "user_id"
     t.integer "payment_id"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 20161107022026) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.text     "location"
-    t.string   "image",      limit: 255
+    t.string   "image"
     t.text     "body"
-    t.string   "slug",       limit: 255
+    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "startdt"
@@ -40,20 +40,20 @@ ActiveRecord::Schema.define(version: 20161107022026) do
   add_index "events", ["slug"], name: "index_events_on_slug", using: :btree
 
   create_table "featured_events", force: :cascade do |t|
-    t.string   "image_url",  limit: 255
-    t.string   "event_url",  limit: 255
+    t.string   "image_url"
+    t.string   "event_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "odr_registrations", force: :cascade do |t|
-    t.string  "email",          limit: 255
-    t.string  "fname",          limit: 255
-    t.string  "lname",          limit: 255
-    t.string  "parish",         limit: 255
+    t.string  "email"
+    t.string  "fname"
+    t.string  "lname"
+    t.string  "parish"
     t.integer "age"
-    t.string  "shirt_size",     limit: 255
-    t.string  "payment_method", limit: 255
+    t.string  "shirt_size"
+    t.string  "payment_method"
     t.text    "diet"
   end
 
@@ -62,79 +62,79 @@ ActiveRecord::Schema.define(version: 20161107022026) do
 
   create_table "payments", force: :cascade do |t|
     t.integer "amount"
-    t.string  "description",  limit: 255
+    t.string  "description"
     t.integer "payable_id"
-    t.string  "payable_type", limit: 255
+    t.string  "payable_type"
     t.boolean "active"
   end
 
   create_table "retreats", force: :cascade do |t|
-    t.string   "fname",                      limit: 255
-    t.string   "lname",                      limit: 255
-    t.string   "email",                      limit: 255
-    t.string   "line1",                      limit: 255
-    t.string   "city",                       limit: 255
-    t.string   "state",                      limit: 255
-    t.string   "zip",                        limit: 255
-    t.string   "phone",                      limit: 255
-    t.string   "gender",                     limit: 255
+    t.string   "fname"
+    t.string   "lname"
+    t.string   "email"
+    t.string   "line1"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "gender"
     t.date     "birthday"
-    t.string   "shirt_size",                 limit: 255
-    t.string   "emergency_contact",          limit: 255
-    t.string   "emergency_contact_relation", limit: 255
-    t.string   "emergency_contact_number",   limit: 255
-    t.string   "insurance_provider",         limit: 255
-    t.string   "insurance_policy_number",    limit: 255
+    t.string   "shirt_size"
+    t.string   "emergency_contact"
+    t.string   "emergency_contact_relation"
+    t.string   "emergency_contact_number"
+    t.string   "insurance_provider"
+    t.string   "insurance_policy_number"
     t.text     "allergy_information"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "transportation",             limit: 255
-    t.boolean  "policy_confirmation",                    default: false
-    t.string   "payment_method",             limit: 255
+    t.string   "transportation"
+    t.boolean  "policy_confirmation",        default: false
+    t.string   "payment_method"
   end
 
   create_table "sermons", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title"
     t.text     "notes"
-    t.string   "audio",      limit: 255
+    t.string   "audio"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "authentication_token",   limit: 255
+    t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "fname",                  limit: 255
-    t.string   "lname",                  limit: 255
-    t.string   "phone",                  limit: 255
-    t.string   "gender",                 limit: 255
+    t.string   "fname"
+    t.string   "lname"
+    t.string   "phone"
+    t.string   "gender"
     t.date     "birthday"
-    t.string   "line1",                  limit: 255
-    t.string   "city",                   limit: 255
-    t.string   "state",                  limit: 255
-    t.string   "zip",                    limit: 255
-    t.string   "shirtsize",              limit: 255
-    t.boolean  "email_contact",                      default: false
-    t.boolean  "facebook_contact",                   default: false
-    t.boolean  "sms_contact",                        default: false
-    t.boolean  "admin",                              default: false
-    t.string   "stripe_id",              limit: 255
-    t.string   "current_last4",          limit: 255
-    t.integer  "status",                             default: 0
+    t.string   "line1"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "shirtsize"
+    t.boolean  "email_contact",          default: false
+    t.boolean  "facebook_contact",       default: false
+    t.boolean  "sms_contact",            default: false
+    t.boolean  "admin",                  default: false
+    t.string   "stripe_id"
+    t.string   "current_last4"
+    t.integer  "status",                 default: 0
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
@@ -143,27 +143,27 @@ ActiveRecord::Schema.define(version: 20161107022026) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "womens_retreat_registrations", force: :cascade do |t|
-    t.string   "fname",                   limit: 255
-    t.string   "lname",                   limit: 255
+    t.string   "fname"
+    t.string   "lname"
     t.date     "birthday"
-    t.string   "address",                 limit: 255
-    t.string   "phone",                   limit: 255
+    t.string   "address"
+    t.string   "phone"
     t.integer  "age"
-    t.string   "academic_classification", limit: 255
-    t.string   "parish",                  limit: 255
+    t.string   "academic_classification"
+    t.string   "parish"
     t.boolean  "accommodations"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "restrictions"
     t.text     "medical_conditions"
     t.text     "insurance_info"
-    t.string   "ec_name",                 limit: 255
-    t.string   "ec_phone",                limit: 255
-    t.string   "ec_relationship",         limit: 255
-    t.string   "payment_method",          limit: 255
-    t.string   "email",                   limit: 255
-    t.string   "shirtsize",               limit: 255
-    t.string   "otherparish",             limit: 255
+    t.string   "ec_name"
+    t.string   "ec_phone"
+    t.string   "ec_relationship"
+    t.string   "payment_method"
+    t.string   "email"
+    t.string   "shirtsize"
+    t.string   "otherparish"
   end
 
 end
