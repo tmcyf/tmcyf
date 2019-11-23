bac9debae92f9772e2168e31b19a328ffaf78a5c
Ivan Varghese <ivanvarghese@me.com>
2019-11-23 17:18:09 -0600

    Updated for 2020 retreat

--- app/controllers/retreat_controller.rb
+++ app/controllers/retreat_controller.rb
@@ -18,7 +18,7 @@ class RetreatController < ApplicationController
 
   def charge
     # Amount in cents
-    amount = 9500
+    amount = 9000
     compensated_charge = StripeCompensator.compensate(amount)
     email = params[:stripeEmail]
 
